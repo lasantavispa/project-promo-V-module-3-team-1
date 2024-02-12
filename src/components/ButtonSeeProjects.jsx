bc64a7cb238fa5d345663ca3fa49703c18c5ba65
Tam-Weibel <tamaraweibel.comgit config --global user.email tamaraweibel@gmail.com>
2024-02-12 20:43:06 +0100

    css updates

--- src/components/ButtonSeeProjects.jsx
+++ src/components/ButtonSeeProjects.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Link} from 'react-router-dom';
 import '../scss/core/Variables.scss';
+import '../scss/core/Mixins.scss';
+import '../scss/layout/Form.scss';
 import '../scss/layout/Header.scss';
 
 function ButtonSeeProjects() {
   return (
-    <Link to="/listProject"className="hero__btn" >
-    <button >Mira tus tarjetas</button>
+    <Link to="/listProject">
+      <button className="buttonImgCrear__large">Mira tus tarjetas</button>
     </Link>
     
   )
